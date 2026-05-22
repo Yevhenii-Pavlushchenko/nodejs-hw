e90a16ba008277b56f0e519baac563b9f2cfdb80
JustFun <paw93@ukr.net>
2026-05-22 16:55:20 +0300

    finished  hw #2
    before  Mentors check

---
+++ src/controllers/notesControllers.js
@@ -0,0 +1,75 @@
+import createError from 'http-errors';
+
+import { Note } from '../models/notes.js';
+
+// Запрос на все нотатки
+export const getAllNotes = async (req, res, next) => {
+  try {
+    const notes = await Note.find();
+    res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Запрос на одну нотатку по адйшке
+export const getNoteById = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+    const note = await Note.findById(noteId);
+
+    if (!note) {
+      throw createError(404, 'Note not found');
+    }
+
+    res.status(200).json(note);
+  } catch (error) {
+    next(error);
+  }
+};
+
+//  Создание новой нотатки
+export const createNote = async (req, res, next) => {
+  try {
+    const newNote = await Note.create(req.body);
+    res.status(201).json(newNote);
+  } catch (error) {
+    next(error);
+  }
+};
+
+//! Удаление нотатки
+export const deleteNote = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+
+    const deletedNote = await Note.findByIdAndDelete(noteId);
+
+    if (!deletedNote) {
+      throw createError(404, 'Note not found');
+    }
+
+    res.status(200).json(deletedNote);
+  } catch (error) {
+    next(error);
+  }
+};
+
+//? Обновление нотатки
+export const updateNote = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+
+    const updatedNote = await Note.findByIdAndUpdate(noteId, req.body, {
+      new: true,
+    });
+
+    if (!updatedNote) {
+      throw createError(404, 'Note not found');
+    }
+
+    res.status(200).json(updatedNote);
+  } catch (error) {
+    next(error);
+  }
+};
